a9dcac69ba4a9d86290772f304577fae1aaa7d02
linoyniazov <linoy2209@gmail.com>
2025-12-24 15:28:24 +0200

    add initial schema for patients, services, and appointments tables

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,7 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT) || 5432,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
