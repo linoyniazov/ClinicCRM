ba71ec8c189c6237332b2f5b566ab75761a5663e
linoyniazov <linoy2209@gmail.com>
2025-12-29 16:15:31 +0200

    add dashboard mock data and use it in dashboard

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,18 +1,23 @@
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 style={{ marginBottom: 24 }}>Dashboard</h1>
+  import DashboardCard from "../components/DashboardCard";
+  import { dashboardMock } from "../mocks/dashboardMock";
 
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "2fr 1fr",
-          gap: 24,
-        }}
-      >
-        <div>LEFT COLUMN</div>
-        <div>RIGHT COLUMN</div>
+  export default function DashboardPage() {
+    const { nextClient } = dashboardMock;
+    return (
+      <div>
+        <h1 style={{ marginBottom: 24 }}>Dashboard</h1>
+
+        <div style={{ display: "grid", gridTemplateColumns: "2fr 1fr", gap: 24 }}>
+          <DashboardCard>
+            <h3>Next Client</h3>
+            <p>
+              <strong>{nextClient.name}</strong>
+            </p>
+            <p>{nextClient.service}</p>
+            <p>{nextClient.time}</p>
+          </DashboardCard>
+          <div />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
