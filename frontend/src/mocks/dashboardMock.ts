fc681ece5de51d3b259576cbaff1883bc1ccefd7
linoyniazov <linoy2209@gmail.com>
2025-12-29 16:41:24 +0200

    ui: extract NextClientCard widget

--- frontend/src/mocks/dashboardMock.ts
+++ frontend/src/mocks/dashboardMock.ts
@@ -1,7 +1,7 @@
 export const dashboardMock = {
     nextClient: {
         name: "Maya Cohen",
-    service: "Acne Treatment",
-    time: "10:30 AM",
+        service: "Acne Treatment",
+        time: "10:30 AM",
     },
 };
